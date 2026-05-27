b7e07a8915859cbd0c035d387e7c7d804a90cc82
alunoalgoritmoseprogramacao <alunoalgoritmos@gmail.com>
2026-05-27 20:28:09 +0000

    Pesquisa

---
+++ 07_Pesquisa/Equals.java
@@ -0,0 +1,10 @@
+
+void main() {
+    
+    String a = IO.readln("Informe um nome... : ");
+    String b = IO.readln("Informe outro nome : ");
+    
+    IO.println("          a.equals(b) = " + a.equals(b));
+    IO.println("a.equalsIgnoreCase(b) = " + a.equalsIgnoreCase(b));
+        
+  }
